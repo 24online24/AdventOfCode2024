6e84e031d8a114b4dc2ae4e798804ed555ea695e
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-12-01 17:55:20 +0200

    Add Day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,51 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::Result;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use AdventOfCode2024::start_day;
+const DAY: &str = "01";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let answer = reader.lines().flatten().count();
+        Ok(answer)
+    }
+
+    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        Ok(0)
+    }
+
+    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+
+    Ok(())
+}
