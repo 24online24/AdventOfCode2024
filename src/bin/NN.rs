97dc4105ca4486f0694430944af532449a317451
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-12-01 17:49:20 +0200

    Use JetBrains template

---
+++ src/bin/NN.rs
@@ -0,0 +1,46 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use anyhow::Result;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use AdventOfCode2024::start_day;
+const DAY: &str = "NN";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+<TEST-INPUT>
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let answer = reader.lines().flatten().count();
+        Ok(answer)
+    }
+
+    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        Ok(0)
+    }
+
+    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn start_day(day: &str) {
+    println!("Advent of Code 2024 - Day {:0>2}", day);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        start_day("00");
+    }
+}
